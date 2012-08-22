07df5e178125f9a2656c9196723ccf7f09610507
Luke Stratman <lstratman@gmail.com>
2012-08-22 17:10:47 -0400

    Added the AC enum.

---
+++ Gdi32/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Win32Interop.Enums
+{
+    public enum AC
+    {
+        AC_SRC_OVER = 0x00,
+        AC_SRC_ALPHA = 0x01
+    }
+}
