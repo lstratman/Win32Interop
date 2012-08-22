f89d117102b09bf999db4c9f2a39dd82d9f88f9b
Luke Stratman <lstratman@gmail.com>
2012-08-22 00:20:18 -0400

    Added the windows hook type enum.

--- User32/Enums.cs
+++ User32/Enums.cs
@@ -14,6 +14,26 @@ namespace Win32Interop.Enums
         SecurityDelegation,
     }
 
+    public enum WH
+    {
+        WH_MSGFILTER = -1,
+        WH_JOURNALRECORD = 0,
+        WH_JOURNALPLAYBACK = 1,
+        WH_KEYBOARD = 2,
+        WH_GETMESSAGE = 3,
+        WH_CALLWNDPROC = 4,
+        WH_CBT = 5,
+        WH_SYSMSGFILTER = 6,
+        WH_MOUSE = 7,
+        WH_HARDWARE = 8,
+        WH_DEBUG = 9,
+        WH_SHELL = 10,
+        WH_FOREGROUNDIDLE = 11,
+        WH_CALLWNDPROCRET = 12,
+        WH_KEYBOARD_LL = 13,
+        WH_MOUSE_LL = 14
+    }
+
     public enum WM : uint
     {
         /// <summary>
